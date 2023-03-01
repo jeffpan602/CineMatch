2aae6077bb0d57782273649b8106c4dd11b4aa91
Jeffrey Pan <107944513+jeffpan602@users.noreply.github.com>
2023-03-01 00:53:56 -0500

    homepage setup

--- cinematch-client/src/App.js
+++ cinematch-client/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import TextField from '@mui/material/TextField';
+import InputAdornment from '@mui/material/InputAdornment';
+import MovieIcon from '@mui/icons-material/Movie';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>CineMatch</h1>
+        <TextField id="outlined-basic" label="Search Movies" variant="outlined" InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <MovieIcon />
+            </InputAdornment>
+          ),
+        }} />
       </header>
     </div>
   );
