5e8baab045203c3440082a8d94b9414260ccf6e5
Jeffrey Pan <107944513+jeffpan602@users.noreply.github.com>
2023-03-01 17:37:34 -0500

    homepage setup

--- cinematch-client/src/App.js
+++ cinematch-client/src/App.js
@@ -1,20 +1,25 @@
 import './App.css';
 import TextField from '@mui/material/TextField';
-import InputAdornment from '@mui/material/InputAdornment';
 import MovieIcon from '@mui/icons-material/Movie';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>CineMatch</h1>
-        <TextField id="outlined-basic" label="Search Movies" variant="outlined" InputProps={{
-          startAdornment: (
-            <InputAdornment position="start">
-              <MovieIcon />
-            </InputAdornment>
-          ),
-        }} />
+        <Container>
+          <Row>
+            <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+              <MovieIcon sx={{ color: 'action.active', mr: 1, my: 2 }} />
+              <TextField id="search-bar" label="Search Movies!" variant="outlined" />
+            </Box>
+            <Button variant="outlined">Submit</Button>
+          </Row>
+        </Container>
       </header>
     </div>
   );
