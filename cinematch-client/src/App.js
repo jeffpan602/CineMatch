0e11f8235f67820f076f7b3720110574b4c5dc1f
Jeffrey Pan <107944513+jeffpan602@users.noreply.github.com>
2023-03-01 17:49:26 -0500

    bootstrap setup

--- cinematch-client/src/App.js
+++ cinematch-client/src/App.js
@@ -2,24 +2,29 @@ import './App.css';
 import TextField from '@mui/material/TextField';
 import MovieIcon from '@mui/icons-material/Movie';
 import Button from '@mui/material/Button';
-import Box from '@mui/material/Box'
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row'
+import { MDBContainer, MDBRow, MDBCol } from 'mdb-react-ui-kit';
+import Box from '@mui/material/Box';
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>CineMatch</h1>
-        <Container>
-          <Row>
-            <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
-              <MovieIcon sx={{ color: 'action.active', mr: 1, my: 2 }} />
-              <TextField id="search-bar" label="Search Movies!" variant="outlined" />
-            </Box>
-            <Button variant="outlined">Submit</Button>
-          </Row>
-        </Container>
+        <MDBContainer>
+          <MDBRow>
+            <MDBCol size='lg'>
+              <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+                <MovieIcon sx={{ color: 'action.active', mr: 1, my: 2 }} />
+                <TextField id="search-bar" label="Search Movies!" variant="outlined" />
+              </Box>
+            </MDBCol>
+            <MDBCol size="sm">
+              <Button variant="outlined">Submit</Button>
+            </MDBCol>
+          </MDBRow>
+        </MDBContainer>
       </header>
     </div>
   );
