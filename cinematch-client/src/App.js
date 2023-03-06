666604f06b5760a322520ea582aa1dc55cff5427
brian xu <blx2@cwru.edu>
2023-03-05 20:33:52 -0500

    just me

--- cinematch-client/src/App.js
+++ cinematch-client/src/App.js
@@ -1,27 +1,39 @@
 import './App.css';
-import TextField from '@mui/material/TextField';
-import MovieIcon from '@mui/icons-material/Movie';
-import Button from '@mui/material/Button';
+import Search from './components/Search'
 import { MDBContainer, MDBRow, MDBCol } from 'mdb-react-ui-kit';
-import Box from '@mui/material/Box';
-
-
+import { useState } from 'react';
 
 function App() {
+  const [state, setState] = useState({
+    s:"",
+    results: [],
+    selected: {}
+  })
+
+  const apikey = "b5d2f69cf0491ce4441c4d04c4befc3d";
+
+  const search = (e) => {
+    if (e.key === "Enter") {
+      
+    }
+  }
+
+  const handleInput = (e) => {
+    let s = e.target.value;
+
+    setState(prevState => {
+      return { ...prevState, s: s}
+    });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>CineMatch</h1>
         <MDBContainer>
           <MDBRow>
-            <MDBCol size='lg'>
-              <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
-                <MovieIcon sx={{ color: 'action.active', mr: 1, my: 2 }} />
-                <TextField id="search-bar" label="Search Movies!" variant="outlined" />
-              </Box>
-            </MDBCol>
-            <MDBCol size="sm">
-              <Button variant="outlined">Submit</Button>
+            <MDBCol>
+            <Search handleInput={handleInput}/>
             </MDBCol>
           </MDBRow>
         </MDBContainer>
