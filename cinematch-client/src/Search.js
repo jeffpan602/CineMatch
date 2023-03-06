ecb4246e7bbc99bebce84d4ad5a00dd504496488
Ulises Soto <uxs58@case.edu>
2023-03-05 20:48:11 -0500

    Updated Pages
    Added pages / as root, and /search?query={searchQuery} to show results

--- cinematch-client/src/Search.js
+++ cinematch-client/src/Search.js
@@ -3,53 +3,60 @@ import React from 'react';
 import CineMatchNavBar from './CineMatchNavBar';
 
 class Search extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      movies: []
-    };
-    this.fetchMovies = this.fetchMovies.bind(this);
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchQuery : "",
+            movies: []
+        };
+        this.fetchMovies = this.fetchMovies.bind(this);
+    }
 
-  fetchMovies(searchQuery) {
-    const apiKey = "b5d2f69cf0491ce4441c4d04c4befc3d";
-    const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&page=1&include_adult=false&query=${searchQuery}`;
+    componentDidMount() {
+        const searchQuery = this.props.location.search.split("=")[1];
+        if (searchQuery) {
+            this.fetchMovies(searchQuery);
+        }
+    }
 
-    fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        this.setState({ movies: data.results });
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  }
+    fetchMovies(searchQuery) {
+        const apiKey = "b5d2f69cf0491ce4441c4d04c4befc3d";
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&page=1&include_adult=false&query=${searchQuery}`;
+        this.setState({ searchQuery: searchQuery });
+        fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            this.setState({ movies: data.results });
+        })
+        .catch(error => {
+            console.error(error);
+        });
+    }
 
-  render() {
-    const { movies } = this.state;
-    return (
-        <div className="Search">
-            <header>
-                {/*<h1>CineMatch</h1>*/}
-                <CineMatchNavBar onSearch={this.fetchMovies}/>
-                {/*Return JSX containing individual movie results*/}
-                <div className="movie-list" style={{display:"flex",flexWrap:"wrap",justifyContent:"center"}}>
-                    {movies && 
-                        movies.filter(movie => movie.poster_path)
-                              .map(movie => (
-                                <div key={movie.id} className="movie">
-                                    <img src={`https://image.tmdb.org/t/p/w185/${movie.poster_path}`} alt={movie.title} />
-                                    <div className="movie-info">
-                                        <h2>{movie.title}</h2>
-                                        <p>{new Date(movie.release_date).getFullYear()}</p>
+    render() {
+        const { movies } = this.state;
+        return (
+            <div className="Search">
+                <header>
+                    <CineMatchNavBar onSearch={this.fetchMovies} searchQuery={this.props.location.search.split("=")[1]} />
+                    {/*Return JSX containing individual movie results*/}
+                    <div className="movie-list" style={{display:"flex",flexWrap:"wrap",justifyContent:"center"}}>
+                        {movies && 
+                            movies.filter(movie => movie.poster_path)
+                                .map(movie => (
+                                    <div key={movie.id} className="movie">
+                                        <img src={`https://image.tmdb.org/t/p/w185/${movie.poster_path}`} alt={movie.title} />
+                                        <div className="movie-info">
+                                            <h2>{movie.title}</h2>
+                                            <p>{new Date(movie.release_date).getFullYear()}</p>
+                                        </div>
                                     </div>
-                                </div>
-                    ))}
-                </div>
-            </header>
-        </div>
-    );
-  }
+                        ))}
+                    </div>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default Search;
--- cinematch-client/src/index.js
+++ cinematch-client/src/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import Search from './Search';
+import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Search />
-  </React.StrictMode>
+ReactDOM.render(
+    <BrowserRouter>
+        <Switch>
+            <Route exact path="/" component={App} />
+            <Route path="/search" component={Search} />
+        </Switch>
+    </BrowserRouter>,
+    document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
