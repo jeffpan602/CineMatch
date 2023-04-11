d1a2eacba710ec946b84218cd227565997e93fb0
Jeffrey Pan <107944513+jeffpan602@users.noreply.github.com>
2023-04-11 17:46:06 -0400

    feat: scoring system of 1-10

--- cinematch-server/django_backend/watched/models.py
+++ cinematch-server/django_backend/watched/models.py
@@ -1,11 +1,17 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 
 
 class watched(models.Model):
     movie_id = models.IntegerField(primary_key=True)
-    rating = models.IntegerField()
+    rating = models.IntegerField(
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(10)
+        ]
+    )
     review = models.TextField()
 
     def _str_(self):
