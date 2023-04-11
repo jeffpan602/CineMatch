5327f822c0704d9fecabbafcbf5194232a0d1422
Joel-Hottinger <jth128@case.edu>
2023-04-11 19:34:20 -0400

    added front end buttons and slider

--- cinematch-client/src/CineMatch.js
+++ cinematch-client/src/CineMatch.js
@@ -1,18 +1,43 @@
 import { Modal, show, Button } from 'react-bootstrap';
 import React, { useState, useEffect } from 'react';
 
+
 const API_IMG = "https://image.tmdb.org/t/p/w500/";
 const API_BASE = "https://api.themoviedb.org/3";
 const API_KEY = "b5d2f69cf0491ce4441c4d04c4befc3d";
 
+
 const CineMatch = ({ title, poster_path, vote_average, release_date, overview, id }) => {
 
+
   const [show, setShow] = useState(false);
+  const [watch_show, setWatchShow] = useState(false);
+  const [seen_show, setSeenShow] = useState(false);
   const [cast, setCast] = useState([]);
   const [director, setDirector] = useState("");
 
+
+
+
+  const [userRating, setUserRating] = useState("5");
+ 
+
+
+
+
   const handleShow = () => setShow(true);
   const handleClose = () => setShow(false);
+  const handleWatchShow = () => { setWatchShow(true); setShow(false);};
+  const handleWatchClose = () => setWatchShow(false);
+  const handleSeenShow = () => { setSeenShow(true); setShow(false);};
+  const handleSeenClose = () => setSeenShow(false);
+
+
+
+
+  const addToWatchList = () => setWatchShow(false); // change this to add movie id to database
+  const addToSeenList = () => setSeenShow(false);  // change this to add movie id and rating to database
+
 
   useEffect(() => {
     const getMovieDetails = async () => {
@@ -21,11 +46,13 @@ const CineMatch = ({ title, poster_path, vote_average, release_date, overview, i
         const data = await res.json();
         const credits = data.credits;
 
+
         // Get directors
         const directors = credits.crew
             .filter((person) => person.job === "Director")
             .map((person) => person.name);
         setDirector(directors);
+
 
         // Get top 5 cast members
         const cast = credits.cast.slice(0, 5).map((person) => person.name);
@@ -35,8 +62,10 @@ const CineMatch = ({ title, poster_path, vote_average, release_date, overview, i
       }
     };
 
+
     getMovieDetails();
   }, [id]);
+
 
   return (
     <div className="card text-center bg-secondary mb-3">
@@ -77,13 +106,57 @@ const CineMatch = ({ title, poster_path, vote_average, release_date, overview, i
               <p>{overview}</p>
             </Modal.Body>
             <Modal.Footer>
+            <Button variant="secondary" onClick={handleSeenShow}>Add to Seen List</Button>
+              <Button variant="secondary" onClick={handleWatchShow}>Add to Watch Later</Button>
               <Button variant="secondary" onClick={handleClose}>Close</Button>
+
+
             </Modal.Footer>
           </Modal>
+          <Modal show={watch_show} onHide={handleWatchClose}>
+            <Modal.Header closeButton>
+              <Modal.Title></Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <h3>Add "{title}" to Watch List?</h3>
+              <br></br>
+              <br></br>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={addToWatchList}>Add to Watch Later</Button>
+              <Button variant="secondary" onClick={handleWatchClose}>Cancel</Button>
+             
+            </Modal.Footer>
+          </Modal>
+
+
+
+
+          <Modal show={seen_show} onHide={handleSeenClose}>
+            <Modal.Header closeButton>
+              <Modal.Title></Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <h3>Add "{title}" to Seen List?</h3>
+              <br></br>
+              <br></br>
+              <h6>My Rating: {userRating}</h6>
+              <input type="range" min="0" max="10" value={userRating} step="1" onChange={(event) => setUserRating(event.target.value)}/>
+             
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={addToSeenList}>Add to Seen List</Button>
+              <Button variant="secondary" onClick={handleSeenClose}>Cancel</Button>
+             
+            </Modal.Footer>
+          </Modal>
+
+
         </div>
       </div>
     </div>
   )
 }
+
 
 export default CineMatch;
