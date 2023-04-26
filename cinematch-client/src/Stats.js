213202b50d0ac807a38cefe6226c8cbfe6869e64
Jeffrey Pan <107944513+jeffpan602@users.noreply.github.com>
2023-04-26 18:56:22 -0400

    updated stats page

--- cinematch-client/src/Stats.js
+++ cinematch-client/src/Stats.js
@@ -7,7 +7,7 @@ import {
 } from 'react-bootstrap';
 import CineMatchNavBar from './CineMatchNavBar';
 import axios from 'axios';
-import { BarChart, Bar, XAxis, YAxis, ResponsiveContainer, CartesianGrid, Tooltip, Legend } from 'recharts';
+import { PieChart, Pie, Legend, Cell, Bar, BarChart, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
 
 const API_KEY = "b5d2f69cf0491ce4441c4d04c4befc3d";
 const BASE_URL = 'https://api.themoviedb.org/3';
@@ -19,13 +19,12 @@ function Stats() {
   const [movieRuntime, setMovieRuntime] = useState([])
   const [topGenres, setTopGenres] = useState([])
   const [topDirectors, setTopDirectors] = useState([])
+  const [ratings, setRatings] = useState([])
+  const [releaseDate, setReleaseDate] = useState([])
 
-  const data = [
-    { name: "Facebook", users: 20 },
-    { name: "Instagram", users: 15 },
-    { name: "Twitter", users: 10 },
-    { name: "Telegram", users: 50 },
-  ];
+  let genreChartData = [];
+  let ratingsChartData = [];
+  const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
 
   useEffect(() => {
     axios.get('http://127.0.0.1:8000/api/watched/')
@@ -37,6 +36,7 @@ function Stats() {
   useEffect(() => {
     const getStats = async () => {
       const movieIds = watched.map(movie => movie.movie_id);
+      const movieRatings = watched.map(movie => movie.rating.toString())
       try {
         const castsResponses = await axios.all(movieIds.map(id => axios.get(`${BASE_URL}/movie/${id}/credits?api_key=${API_KEY}`)));
         const responses = await axios.all(movieIds.map(id => axios.get(`${BASE_URL}/movie/${id}?api_key=${API_KEY}`)));
@@ -44,6 +44,7 @@ function Stats() {
         const runtime = responses.map(response => response.data.runtime).flat()
         const casts = castsResponses.map(response => response.data.cast).flat();
         const crew = castsResponses.map(response => response.data.crew).flat();
+        const releaseDate = responses.map(response => response.data.release_date).flat()
 
         const actors = casts.reduce((acc, cast) => {
           const actorName = cast.name;
@@ -71,13 +72,31 @@ function Stats() {
           return gen;
         }, {});
 
+        const ratings = movieRatings.reduce((rat, currRat) => {
+          const ratingVal = currRat
+          rat[ratingVal] = rat[ratingVal] ? rat[ratingVal] + 1 : 1;
+          return rat;
+        }, {});
+
+        const decades = releaseDate.reduce((map, date) => {
+          const year = parseInt(date.split("-")[0]);
+          const decade = Math.floor(year / 10) * 10;
+          if (!map[decade]) {
+            map[decade] = 1;
+          } else {
+            map[decade]++;
+          }
+          return map;
+        }, {});
+
         const sortedActors = Object.entries(actors).sort((a, b) => b[1] - a[1]).slice(0, 10);
-        setTopActors(sortedActors);
         const sortedDirectors = Object.entries(directors).sort((a, b) => b[1] - a[1]).slice(0, 10);
+        setTopActors(sortedActors);
         setTopDirectors(sortedDirectors)
         setMovieRuntime(totalRuntime)
-        const numGenres = Object.entries(genres)
-        setTopGenres(numGenres)
+        setTopGenres(genres)
+        setRatings(ratings)
+        setReleaseDate(decades)
       } catch (error) {
         console.error(error);
       }
@@ -85,15 +104,53 @@ function Stats() {
     getStats();
   }, [watched]);
 
+  for (let num = Object.entries(topGenres).length; num >= 0; num--) {
+    genreChartData.push({
+      genre: Object.keys(topGenres)[num],
+      count: Object.values(topGenres)[num]
+    })
+  }
+
+  for (let num = Object.entries(ratings).length; num >= 0; num--) {
+    ratingsChartData.push({
+      rating: Object.keys(ratings)[num],
+      count: Object.values(ratings)[num]
+    })
+  }
+
+  const [mostWatchedDecade, count] = Object.entries(releaseDate).reduce(
+    ([decade, count], [currDecade, currCount]) => {
+      if (currCount > count) {
+        return [currDecade, currCount];
+      }
+      return [decade, count];
+    },
+    ["", 0]
+  );
+  console.log([mostWatchedDecade, count])
+
+  const RADIAN = Math.PI / 180;
+  const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+    return (
+      <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+        {`${(percent * 100).toFixed(0)}%`}
+      </text>
+    );
+  };
+
   return (
     <>
       <CineMatchNavBar setMovies={setMovies} />
-      <div className="stats-page">
-        <span className="titleSpan">
-          <h1>Stats</h1>
-        </span>
+      <span className="titleSpan">
+        <h1>Stats</h1>
+      </span>
 
-        <div className='stats-tablecontainer'>
+      <div class="container">
+        <div>
           <Table className='statsTable'>
             <thead>
               <tr>
@@ -110,7 +167,38 @@ function Stats() {
               ))}
             </tbody>
           </Table>
+        </div>
 
+        <div>
+          <PieChart width={400} height={400}>
+            <Legend layout="horizontal" verticalAlign="bottom" align="middle" ></Legend>
+            <Pie
+              data={genreChartData}
+              cx="50%"
+              cy="50%"
+              labelLine={false}
+              label={renderCustomizedLabel}
+              outerRadius={140}
+              fill="#8884d8"
+              dataKey="count"
+              nameKey="genre"
+              legendType='rect'
+            >
+              {genreChartData.map((entry, index) => (
+                <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+              ))}
+            </Pie>
+          </PieChart>
+        </div>
+
+        <div>
+          <h1 align="center">Total Runtime Watched:</h1>
+          <h1 align="center">{movieRuntime}</h1>
+        </div>
+      </div>
+
+      <div className='container'>
+        <div>
           <Table className='statsTable'>
             <thead>
               <tr>
@@ -129,23 +217,31 @@ function Stats() {
           </Table>
         </div>
 
-        <ResponsiveContainer className="stats-chart" width={500} height={300}>
+        <div>
           <BarChart
-            data={data}
+            width={500}
+            height={300}
+            data={ratingsChartData}
             margin={{
               top: 5,
               right: 30,
               left: 20,
               bottom: 5,
-            }}>
+            }}
+          >
             <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" tick={{fill: '#d9dbe0'}}/>
-            <YAxis tick={{fill: '#d9dbe0'}}/>
-            <Tooltip/>
+            <XAxis dataKey="rating" label={{ value: 'Movie Ratings', position: 'insideBottomRight', offset: 0 }} />
+            <YAxis />
+            <Tooltip />
             <Legend />
-            <Bar dataKey="users" fill="#3b71f8" />
+            <Bar dataKey="count" fill="#D6B85A" />
           </BarChart>
-        </ResponsiveContainer>
+        </div>
+
+        <div>
+          <h1 align="center">Most Watched Decade:</h1>
+          <h1 align="center">{mostWatchedDecade}'s</h1>
+        </div>
       </div>
     </>
   );
